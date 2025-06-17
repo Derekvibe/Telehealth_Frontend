1e19114e6306ab86bfa38fc63460954b4b1df361
Derek Emmanuel <derekemmanuel99@gmail.com>
2025-06-17 12:53:27 +0100

    form input colors

--- src/pages/Auth/Join/NewUser.jsx
+++ src/pages/Auth/Join/NewUser.jsx
@@ -72,7 +72,7 @@ function NewUser() {
                 placeholder={`Enter ${field === 'passwordConfirm' ? 'Confirm Password' : field}`}
                 value={formData[field]}
                 onChange={handleChange}
-                className="w-full px-4 py-2 border border-black rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 text-black"
+                className="w-full px-4 py-2 border border-black rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 text-white bg-gray-600"
               />
             </div>
           ))}
